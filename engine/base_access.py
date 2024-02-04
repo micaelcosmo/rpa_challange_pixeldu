a5b72bc0b5213287e9c54b7bc3729b2615fca999
Micael Cosmo <micael.trabalho@hotmail.com>
2024-02-04 14:09:08 -0300

    refactor/feat: Changing engine for RPA
    This update change used selenium while make access to selenium from rpaframework, like webdriver too.

--- engine/base_access.py
+++ engine/base_access.py
@@ -1,32 +1,44 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
+from logger import AppLogger
+
+from RPA.core.webdriver import download, start, webdriver
+
 
 class URLAccess:
-    def __init__(self, url):
-        self.url = url
-        self.service = Service(ChromeDriverManager().install())
-        self.options = self.config_options()
-        self.driver = webdriver.Chrome(service=self.service, options=self.options)
-        self.access_url()
-
+    def __init__(self, url:str='', filename_screenshot:str=''):
+        self.driver = None
+        self.list_news = []
+        self.logger = AppLogger().logger
+        self.set_webdriver()
+        self.set_chrome_options()
+        self.open_url(url=url, screenshot=filename_screenshot)
+        
     @property
     def driver_browser(self):
         return self.driver
-        
-    def config_options(self):
-        options = Options()
-        options.add_argument("--ignore-certificate-errors") # Ignore certificate errors
-        options.add_argument("--ignore-ssl-errors") # Ignore SSL errors
-        options.add_argument("--disable-blink-features=AutomationControlled") # Remove "automated process.." flag
-        # options.add_argument("--headless") # Run Chrome in headless mode
-        return options
     
-    def access_url(self):
-        # Open the browser and access the URL
-        self.driver.get(self.url)
+    def set_chrome_options(self):
+        options = webdriver.ChromeOptions()
+        options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
+        options.add_argument("--disable-extensions")
+        options.add_argument("--disable-gpu")
+        options.add_argument('--disable-web-security')
+        options.add_argument("--start-maximized")
+        options.add_argument('--remote-debugging-port=9222')
+        options.add_argument("--log-level=3") 
+        return options
 
-    def close_browser(self):
-        # Close the browser
-        self.driver.quit()
+    def set_webdriver(self, browser:str="Chrome"):
+        options = self.set_chrome_options()
+        executable_driver_path = download(browser)
+        self.logger.warning("Using downloaded driver: %s" % executable_driver_path)
+        self.driver = start(browser=browser,  options=options)
+       
+    def open_url(self, url:str, screenshot:str=None):
+        self.driver.get(url)
+        if screenshot:
+            self.driver.get_screenshot_as_file(screenshot)
+
+    def driver_quit(self):
+        if self.driver:
+            self.driver.quit()
