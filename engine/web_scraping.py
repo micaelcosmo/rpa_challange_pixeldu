6a5f7f03a4cfb46a6f3a2977f50976b81587feba
Micael Cosmo <micael.trabalho@hotmail.com>
2024-02-05 08:03:04 -0300

    doc/feat/refactor: Docstrings, Pagination and Counters
    Added docstrings to explain something about classes. A new feat is added to process other pages. And refactored how it count prhases, using now 'nltk' lib for this step.

--- engine/web_scraping.py
+++ engine/web_scraping.py
@@ -7,6 +7,10 @@ import pandas as pd
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import nltk
+
+
+nltk.download('punkt')
 
 
 @dataclass
@@ -20,13 +24,46 @@ class News:
 
 
 class WebScraping:
-    def __init__(self, url:str='', search:str='', filename_excel:str='', filename_screenshot:str='', max_elements:int=10):
+    """
+    A class for performing web scraping tasks.
+
+    Args:
+        url (str): The URL of the website to scrape.
+        search (str): The search term to use for scraping.
+        filename_excel (str): The filename to save the scraped data in Excel format.
+        filename_screenshot (str): The filename to save the screenshots.
+        max_elements_by_page (int): The maximum number of elements to scrape per page, if value is equal -1 will process all.
+        max_pages (int): The maximum number of pages to scrape, if value is equal -1 will process all.
+
+    Attributes:
+        access (URLAccess): An instance of the URLAccess class for accessing the website.
+        logger (Logger): An instance of the AppLogger class for logging.
+        search_word (str): The search term.
+        filename_excel (str): The filename for saving the scraped data.
+        max_elements (int): The maximum number of elements to scrape per page.
+        max_pages (int): The maximum number of pages to scrape.
+        driver (WebDriver): The web driver instance.
+        list_news (list): A list to store the scraped news.
+
+    Methods:
+        _run(): Runs the web scraping process.
+        store_news(): Stores the scraped news.
+        is_a_finance_news(text): Checks if the given text is related to finance news.
+        save_picture_to_file(filename, picture): Saves the picture to a file.
+        to_snake_case(content_text): Converts the given text to snake case.
+        search(): Performs the search operation.
+        news_to_excel(): Saves the scraped news to an Excel file.
+        search_for_pagination(): Checks if the search has pagination.
+        next_page(): Moves to the next page.
+    """
+    def __init__(self, url:str='', search:str='', filename_excel:str='', filename_screenshot:str='', max_elements_by_page:int=-1, max_pages:int=1):
         try:
             self.access = URLAccess(url=url, filename_screenshot=filename_screenshot)
             self.logger = AppLogger().logger
             self.search_word = search
             self.filename_excel = filename_excel
-            self.max_elements = max_elements
+            self.max_elements = max_elements_by_page
+            self.max_pages = 9999999 if max_pages == -1 else max_pages
             self.driver = self.access.driver_browser
             self.list_news = []
             self._run()
@@ -34,13 +71,28 @@ class WebScraping:
             self.access.driver_quit()
 
     def _run(self):
+        """
+        Runs the web scraping process.
+        """
         self.logger.info("Running web scraping process")
         self.search()
-        self.store_news()
+        for i in range(self.max_pages):
+            self.logger.info(f"Processing page {i+1}")
+            if self.search_for_pagination():
+                self.logger.info("This search has pagination")
+                self.store_news()
+                self.next_page()
+            else:
+                self.logger.info("This search does not have pagination")
+                self.store_news()
+                break
         self.news_to_excel()
         self.logger.info("Web scraping process finished")
 
     def store_news(self):
+        """
+        Stores the scraped news.
+        """
         self.logger.info("Storing news")
         results = WebDriverWait(self.driver, 0.1).until(
             EC.visibility_of_element_located((By.CLASS_NAME, "SearchResultsModule-results"))
@@ -58,7 +110,7 @@ class WebScraping:
             try:
                 description_div = element.find_element(By.CLASS_NAME, "PagePromo-description")
                 description = description_div.find_element(By.CLASS_NAME, "PagePromoContentIcons-text").text
-                number_of_prhases = description.count(".")
+                number_of_prhases = len(nltk.sent_tokenize(description))
             except:
                 description = 'No description found'
                 number_of_prhases = 0
@@ -83,11 +135,30 @@ class WebScraping:
             self.list_news.append(news)
 
     def is_a_finance_news(self, text):
+        """
+        Checks if the given text is related to finance news.
+
+        Args:
+            text (str): The text to check.
+
+        Returns:
+            bool: True if the text is related to finance news, False otherwise.
+        """
         currency_symbols = ["$", "€", "£", "¥", "USD", "EUR", "GBP", "JPY"]
         pattern = re.compile(fr'\b({"|".join(map(re.escape, currency_symbols))})?\s?\d+(\d+)?\b')
         return bool(pattern.search(text))
     
     def save_picture_to_file(self, filename, picture):
+        """
+        Saves the picture to a file.
+
+        Args:
+            filename (str): The filename to save the picture.
+            picture (WebElement): The picture element.
+
+        Returns:
+            str: The path of the saved picture file.
+        """
         if picture is None:
             return 'No picture found'
         path = f'data/{filename}'
@@ -95,9 +166,21 @@ class WebScraping:
         return path
 
     def to_snake_case(self, content_text:str):
+        """
+        Converts the given text to snake case.
+
+        Args:
+            content_text (str): The text to convert.
+
+        Returns:
+            str: The converted text in snake case.
+        """
         return '_'.join(re.sub('([A-Z][a-z]+)', r' \1', re.sub('([A-Z]+)', r' \1', content_text.replace('-', ' ').replace("'", "").replace("`s", ""))).split()).lower()
 
     def search(self):
+        """
+        Performs the search operation.
+        """
         search_button = self.driver.find_element(By.CLASS_NAME, "SearchOverlay-search-button")
         search_button.click()
         self.driver.implicitly_wait(5)
@@ -107,6 +190,34 @@ class WebScraping:
         self.driver.implicitly_wait(5)
         
     def news_to_excel(self):
+        """
+        Saves the scraped news to an Excel file.
+        """
         df = pd.DataFrame([news.__dict__ for news in self.list_news])
         df.to_excel(self.filename_excel, index=False)
         self.logger.info(f"News saved to {self.filename_excel}")
+
+    def search_for_pagination(self):
+        """
+        Checks if the search has pagination.
+
+        Returns:
+            bool: True if the search has pagination, False otherwise.
+        """
+        try:
+            self.driver.find_element(By.CLASS_NAME, "Pagination")
+            return True
+        except Exception as e:
+            self.logger.error(f"This search does not have pagination")
+            return False
+
+    def next_page(self):
+        """
+        Moves to the next page.
+        """
+        self.logger.info("Searching for next page")
+        try:
+            next_page_link = self.driver.find_element(By.CSS_SELECTOR, 'div.Pagination-nextPage a')
+            next_page_link.click()
+        except Exception as e:
+            self.logger.error(f"Error clicking next page: {e}, maybe it does not work for this page")
