c39ebc9852f08b2b667ac58c1abe66d74a59c343
Micael Cosmo <micael.trabalho@hotmail.com>
2024-02-06 20:54:47 -0300

    refactor: Reduced process
    Now the main does not more configure many constants and call only one task.

--- main.py
+++ main.py
@@ -1,40 +1,10 @@
-from logger import AppLogger
-from engine.web_scraping import WebScraping
+from Tasks import Tasks
 
+def main():
+    # Create an instance of the Tasks class
+    tasks = Tasks
 
-NEWS_URL= "https://apnews.com/" # URL to scrape
-SCREENSHOT_NAME = "apnews.png"  # Screenshot file name
-EXCEL_NAME = "apnews.xlsx"      # Excel file name
-SEARCH_WORLD = "money"          # Search term
-MAX_ELEMENTS_TO_PROCESS = -1    # Max elements to process | -1 to process all
-MAX_PAGES_TO_PROCESS = -1       # Max pages to process | -1 to process all
-MONTHS_RANGE = 3                # Months reach to filter by date | 0 or 1 to process only current month
+    tasks.rpa_challenge(months_range=0, search_phrase="money")
 
-logger = AppLogger().logger
-
-def minimal_task():
-    """
-    Performs a minimal task of web scraping news articles from a given URL,
-    filtering by a specific search term, and saving the results to an Excel file
-    and taking a screenshot of the webpage.
-
-    Args:
-        None
-
-    Returns:
-        None
-    """
-    logger.info("Starting minimal task")
-    WebScraping(
-        url=NEWS_URL, 
-        search=SEARCH_WORLD, 
-        filename_excel=EXCEL_NAME, 
-        filename_screenshot=SCREENSHOT_NAME, 
-        max_elements_by_page=MAX_ELEMENTS_TO_PROCESS,
-        max_pages=MAX_PAGES_TO_PROCESS,
-        months_range=MONTHS_RANGE)
-    logger.info("Ending minimal task")
-
-
-if __name__ == "__main__":
-    minimal_task()
+if __name__ == '__main__':
+    main()
